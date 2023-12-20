af9eb8bca40a898d652398fa1fd6939a88e898a2
soulthanfb <fadhlillahbagussoulthan@gmail.com>
2023-12-20 23:56:25 +0700

    Add 3 data mobil

---
+++ Ferrari.java
@@ -0,0 +1,15 @@
+public class Ferrari extends Company{  
+    @Override  
+        public String price(){   
+                        return "RP. 5.500.000.000";  
+      }  
+    @Override  
+    public int year()
+    {
+        return 2023;
+    }
+    @Override  
+    public String pack(){  
+             return "Ferrari Purosangue";  
+        }         
+}//End of the Polytron class. 
